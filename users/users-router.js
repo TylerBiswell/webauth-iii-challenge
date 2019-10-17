2a5a47f405c752d7962493d34397b42c3f73050b
Tyler Biswell <tsbiswell@gmail.com>
2019-10-17 10:18:31 -0600

    rawr its middleware

--- users/users-router.js
+++ users/users-router.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -8,17 +7,58 @@ const restricted = require('../auth/restricted-middleware');
 
 // POST /api/register endpoint
 router.post('/register', (req, res) => {
-  res.send('Hello from POST /api/register endpoint');
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      const token = generateToken(saved);
+
+      res.status(201).json({ user: saved, token });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 // POST /api/login endpoint
 router.post('/login', (req, res) => {
-  res.send('Hello from POST /api/login endpoint');
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome, ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid credentials' });
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 // GET /api/users endpoint
 router.get('/users', restricted, (req, res) => {
   res.send('Hello from GET /api/users endpoint');
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    department: user.department,
+  };
+  const options = {
+    expiresIn: '1d',
+  };
+  return jwt.sign(payload, process.env.JWT_SECRET, options);
+}
 
 module.exports = router;
