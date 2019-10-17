c426b050a0cdfffcfddbef1077ec08bde82cc9cc
Tyler Biswell <tsbiswell@gmail.com>
2019-10-17 10:17:23 -0600

    set up users-model

--- users/users-router.js
+++ users/users-router.js
@@ -1,3 +1,24 @@
 const router = require('express').Router();
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('./users-model');
+const restricted = require('../auth/restricted-middleware');
+
+// POST /api/register endpoint
+router.post('/register', (req, res) => {
+  res.send('Hello from POST /api/register endpoint');
+});
+
+// POST /api/login endpoint
+router.post('/login', (req, res) => {
+  res.send('Hello from POST /api/login endpoint');
+});
+
+// GET /api/users endpoint
+router.get('/users', restricted, (req, res) => {
+  res.send('Hello from GET /api/users endpoint');
+});
+
 module.exports = router;
